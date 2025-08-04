d83b958e4c3045d9bcd2c7054593d5442e45e749
onlylrs <rliuar@connect.ust.hk>
2025-08-04 16:04:25 +0800

    edit run training.sh

--- run_training.sh
+++ run_training.sh
@@ -12,7 +12,7 @@ export DATASETS_VERBOSITY=warning
 
 # the following 2 lines are only for dealing with cuda out of mem issues - use GPU from 6
 # should be disabled when no one is using the GPU
-export CUDA_VISIBLE_DEVICES=6  # 使用空闲的GPU 6
+export CUDA_VISIBLE_DEVICES=7  # 使用空闲的GPU 6
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True  # 减少内存碎片
 
 echo "=== 开始训练 ==="
@@ -27,12 +27,12 @@ python -u scripts/train/train_jina.py \
     --train_data data0/train.jsonl \
     --data_format jsonl \
     --config_file configs/jina_training_config.json \
-    --fp16 \
+    --bf16 \
     --num_train_epochs 1 \
     --per_device_train_batch_size 1 \
     --learning_rate 2e-5 \
     --max_seq_length 256 \
-    --output_dir /project/fyp25_hc2/results/jina_test_run \
+    --output_dir /project/fyp25_hc2/results/jina_test_run0 \
     --use_lora \
     --lora_r 16 \
     --lora_alpha 16 2>&1 | tee training_output.log
